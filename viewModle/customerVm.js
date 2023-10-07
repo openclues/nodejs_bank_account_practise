677ebd2e39fe92dfa9d9d9f4ef47689da9aabf92
openclues <58612890+openclues@users.noreply.github.com>
2023-10-07 13:35:05 +0300

    initial

---
+++ viewModle/customerVm.js
@@ -0,0 +1,11 @@
+let customerViewModle = {
+    customers : [
+        {
+            custName : "islam", 
+            custImage : "../images/2.jpg"
+        }
+    ]
+}
+
+
+ko.applyBindings(customerViewModle)
